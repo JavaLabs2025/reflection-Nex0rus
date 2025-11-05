292022284033947e912d9f8216138ed8d7b749ff
Gogolev Andrey <nerud1502@gmail.com>
2025-11-05 22:43:53 +0300

    demonstration

--- src/main/java/org/example/GenerateExample.java
+++ src/main/java/org/example/GenerateExample.java
@@ -17,22 +17,22 @@ public class GenerateExample {
     public static void main(String[] args) {
         var gen = new Generator("org.example");
         try {
-            // System.out.println("Generating Example: " + gen.generateValueOfType(Example.class));
+            System.out.println("Generating Example: " + gen.generateValueOfType(Example.class));
 
-            // System.out.println("Generating Triangle: " + gen.generateValueOfType(Triangle.class));
-            // System.out.println("Generating Rectangle: " + gen.generateValueOfType(Rectangle.class));
-            // System.out.println("Generating Shape: " + gen.generateValueOfType(Shape.class));
+            System.out.println("Generating Triangle: " + gen.generateValueOfType(Triangle.class));
+            System.out.println("Generating Rectangle: " + gen.generateValueOfType(Rectangle.class));
+            System.out.println("Generating Shape: " + gen.generateValueOfType(Shape.class));
 
-            // System.out.println("Generating Product: " + gen.generateValueOfType(Product.class));
+            System.out.println("Generating Product: " + gen.generateValueOfType(Product.class));
             System.out.println("Generating Cart: " + gen.generateValueOfType(Cart.class));
 
-            // System.out.println("Generating BinaryTreeNode: " + gen.generateValueOfType(BinaryTreeNode.class));
+            System.out.println("Generating BinaryTreeNode: " + gen.generateValueOfType(BinaryTreeNode.class));
 
-            // ProductService productService = (ProductService) gen.generateValueOfType(ProductService.class);
-            // System.out.println(
-            //        "Generating ProductService: " + productService +
-            //        ". Calling getProduct(): " + productService.getProduct()
-            // );
+            ProductService productService = (ProductService) gen.generateValueOfType(ProductService.class);
+            System.out.println(
+                    "Generating ProductService: " + productService +
+                    ". Calling getProduct(): " + productService.getProduct()
+            );
         } catch (Throwable e) {
             throw new RuntimeException(e);
         }
